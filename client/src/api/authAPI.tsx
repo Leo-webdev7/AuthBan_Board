a1623eb9a20d5979fea6bceb152913fc46b9b295
Leo <lshapirov2@gmail.com>
2024-10-31 15:54:32 -0500

    login error fixed

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,13 +1,16 @@
 import { UserLogin } from "../interfaces/UserLogin";
 import axios from 'axios';
 
+const API_URL = 'http://localhost:3000/auth'
+
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
   try {
-    const response = await axios.post('/login', {
+    const response = await axios.post(`${API_URL}/login`, {
       username: userInfo.username,
       password: userInfo.password
     });
+    console.log(JSON.stringify(response))
     return response.data;
   } catch (error) {
     console.error('Error logging in:', error)
